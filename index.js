a7efbbce9f4ad9cdc09dec1f5069dcf3073f5bf2
Adnan0061 <adnan.ahmed0061@gmail.com>
2021-12-07 13:19:38 +0600

    without jwt

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
-let admin = require("firebase-admin");
+// let admin = require("firebase-admin");
 require('dotenv').config()
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
@@ -10,11 +10,11 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 const port = process.env.PORT || 5000;
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
 
 
 //middleware
@@ -26,20 +26,20 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.70s8n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function verifyToken(req, res, next) {
-  if(req.headers?.authorization?.startsWith('Bearer ')){
-    const token = req.headers.authorization.split(' ')[1]
+// async function verifyToken(req, res, next) {
+//   if(req.headers?.authorization?.startsWith('Bearer ')){
+//     const token = req.headers.authorization.split(' ')[1]
     
-    try {
-      const decodedUser = await admin.auth().verifyIdToken(token);
-      req.decodedEmail = decodedUser.email;
-    }
-    catch{
+//     try {
+//       const decodedUser = await admin.auth().verifyIdToken(token);
+//       req.decodedEmail = decodedUser.email;
+//     }
+//     catch{
 
-    }
-  }
-  next();
-}
+//     }
+//   }
+//   next();
+// }
 
 async function run() {
   try {
